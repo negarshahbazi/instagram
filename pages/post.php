3a9a85e91b662fc38ab32fc84f2180bc40254c83
john Doe <negarshahbazi.official@gmail.com>
2024-01-04 11:02:31 +0100

    forth

--- pages/post.php
+++ pages/post.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-var_dump($_SESSION['srcMyPost']);
+// var_dump($_SESSION['srcMyPost']);
 ?>
 
 
@@ -16,18 +16,18 @@ var_dump($_SESSION['srcMyPost']);
 </head>
 
 <body>
-    <form action="../process/back_end.php" method="post">
+    <form action="../process/back_end.php" method="post"  role="form" id="contact_form" class="contact-form" enctype="multipart/form-data">
         <div class="container d-flex justify-content-center align-items-center">
             <div class="card m-5" style="width: 40rem;">
                 <h4 class="text-center m-5">choisir votre photo:</h4>
                 <div class="text-center m-5">
-                <!-- <?php  move_uploaded_file($image["tmp_name"],  $_SESSION['srcMyPost']); ?> -->
-                    <!-- <?php  move_uploaded_file( '',  $_SESSION['srcMyPost']); ?> -->
-                    <img src="<?php echo $_SESSION['srcMyPost']?>" alt="">
+                    <img src="" alt="">
+                    <!-- aller vers back_end ici  -->
                     <input class="" type="file" name="srcMyPost">
                 </div>
                 <div class="card-body text-center">
-                    <p class="card-text text-center  "><button type="submit" name="postMyImage">Envoyer</button></p>
+                    <p class="card-text text-center  ">
+                        <button type="submit" name="postMyImage">Envoyer</button></p>
                 </div>
             </div>
         </div>
--- process/back_end.php
+++ process/back_end.php
@@ -1,36 +1,50 @@
-<!-- <?php
-session_start();
-if(isset($_POST['srcMyPost']) && !empty($_POST['srcMyPost']) && isset($_POST['postMyImage'])){
-    $_SESSION['srcMyPost']=$_POST['srcMyPost'];
-   
-header('Location: ../pages/post.php');
-}
-?> -->
 
 <?php
-if(isset($_FILES['srcMyPost'])){
-$errors= array();
-$file_name = $_FILES['srcMyPost']['name'];
-$file_size =$_FILES['srcMyPost']['size'];
-$file_tmp =$_FILES['srcMyPost']['tmp_name'];
-$file_type=$_FILES['srcMyPost']['type'];
-$file_ext=strtolower(end(explode('.',$_FILES['srcMyPost']['name'])));
+session_start();
 
-$extensions= array("jpeg","jpg","png");
+if(isset($_POST['postMyImage'])) {
+    
+    var_dump($_FILES['srcMyPost']);
+    $errors = array();
+    $file_name = $_FILES['srcMyPost']['name'];
+    $file_size = $_FILES['srcMyPost']['size'];
+    $file_tmp = $_FILES['srcMyPost']['tmp_name'];
+    $file_type = $_FILES['srcMyPost']['type'];
+    $file_ext = strtolower(end(explode('.', $_FILES['srcMyPost']['name'])));
 
-if(in_array($file_ext,$extensions)=== false){
-$errors[]="extension not allowed, please choose a JPEG or PNG file.";
-}
+    $extensions = array("jpeg", "jpg", "png");
 
-if($file_size > 2097152){
-$errors[]='File size must be excately 2 MB';
-}
+    if (in_array($file_ext, $extensions) === false) {
+        $errors[] = "extension not allowed, please choose a JPEG or PNG file.";
+    }
 
-if(empty($errors)==true){
-move_uploaded_file($file_tmp,"images/".$file_name);
-echo "Success";
-}else{
-print_r($errors);
+    if ($file_size > 2097152) {
+        $errors[] = 'File size must be excately 2 MB';
+    }
+
+    if (empty($errors) == true) {
+       
+        $resultat = move_uploaded_file($file_tmp, "images/" . $file_name);
+        echo "Success";
+    } else {
+        
+        print_r($errors);
+    }
+
+
+require_once('./connexion.php');
 }
-}
+// $request = $database->query("SELECT * FROM user WHERE pseudo=" . $_SESSION['pseudo']);
+// // $request->fetch
+// if ($resultat) {
+//     $request = $database->prepare('INSERT INTO post (src_photo) VALUES (:src_photo)');
+//     $request->execute([
+//         ' :src_photo' => $resultat,
+//     ]);
+//     //  header("Location: profil.php?id=".$_SESSION['id']);
+//     //  header("Location: profil.php");
+// }
 ?>
+   
+
+                     
