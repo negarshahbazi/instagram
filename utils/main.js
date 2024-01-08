488fce05b83d0a5d69d3bb6e554bb7d066fdf75d
john Doe <negarshahbazi.official@gmail.com>
2024-01-08 16:54:47 +0100

    jkj

--- utils/main.js
+++ utils/main.js
@@ -35,3 +35,36 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Like button not found");
   }
 });
+
+
+// comment modal
+document.addEventListener("DOMContentLoaded", function () {
+  const commentModalElement = document.getElementById("commentModal");
+  // const commentModal2Element = document.getElementById("commentModal2");
+
+  // Show comment modal 1
+  commentModalElement.addEventListener("click", function () {
+     $("#commentModal").modal("show");
+  });
+
+  // Show comment modal 2
+  // commentModal2Element.addEventListener("click", function () {
+  //    $("#commentModal2").modal("show");
+  // });
+});
+document.getElementById('comment').addEventListener('click',()=>{
+  document.getElementById('boxOfCommentaire').className="visible";
+
+}
+)
+
+// close
+const closeButtons = document.getElementsByClassName('close');
+
+// Using Array.from to convert the collection to an array
+Array.from(closeButtons).forEach(function (button) {
+  button.addEventListener('click', () => {
+    $("#commentModal").modal("hide");
+
+  });
+});
